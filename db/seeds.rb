f904290a9c41f4eb0dcfc0d2b40984a85d29ed12
Paul Portier <paulportier@gmail.com>
2019-11-13 16:53:01 +0100

    appli fonctionnelle, front turbo degueulasse... work in progress on va dire

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'PNY',
+    address:      'Rue Oberkampf',
+    category:     'french',
+    phone_number: '0101010101'
+  },
+  {
+    name:         'Pizza East',
+    address:      'Rue du Bled',
+    category:     'belgian',
+    phone_number: '0101010101'
+  },
+  {
+    name:         'La tour du bled',
+    address:      'Rue des bois',
+    category:     'japanese',
+    phone_number: '0101010101'
+  },
+  {
+    name:         'Mamma Roma',
+    address:      'Rue du moulin',
+    category:     'italian',
+    phone_number: '0101010101'
+  },
+  {
+    name:         'Pizza West',
+    address:      'Rue du Marché',
+    category:     'italian',
+    phone_number: '0101010101'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
